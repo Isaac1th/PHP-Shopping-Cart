7d806fae26e32177dc22ca125e3b25bada698a53
Isaac <you@example.com>
2022-05-15 19:48:11 +0300

    db setup, use component function on index.php to avoid repetitiveness

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<?php
+
+require_once ('./php/Database.php');
+require_once ('./php/component.php');
+
+
+// Create instance of Database class
+$database = new Database(dbName:"ProductDB", tableName:"ProductTable", password:"", serverName:"localhost", userName:"root");
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,40 +20,19 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- BS CDN -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="./css/styles.css">
 </head>
 <body>
 
 <div class="container">
     <div class="row text-center py-5">
-        <div class="col-md-3 col-sm-6 my-3 my-md-0">
-            <form action="index.php" method="post">
-                <div class="card shadow">
-                    <div>
-                        <img src="./upload/product1.png" alt="img1" class="img-fluid card-img-top">
-                    </div>
-                    <div class="card-body">
-                        <h5 class="card-title">Product1</h5>
-                        <h6>
-                            <i class="fas fa-star"></i>
-                            <i class="fas fa-star"></i>
-                            <i class="fas fa-star"></i>
-                            <i class="fas fa-star"></i>
-                            <i class="far fa-star"></i>
-                        </h6>
-                        <p class="card-text">
-                            Sample text
-                        </p>
-                        <h5>
-                            <small><s>$519</s></small>
-                            <span class="price">$599</span>
-                        </h5>
-                    </div>
-                </div>
-            </form>
-        </div>
-        <div class="col-md-3 col-sm-6 my-3 my-md-0"></div>
-        <div class="col-md-3 col-sm-6 my-3 my-md-0"></div>
-        <div class="col-md-3 col-sm-6 my-3 my-md-0"></div>
+        <?php 
+            component(productName:"Product1", productPrice:'$599', productImg: "./upload/product1.png");
+            component(productName:"Product2", productPrice:'$99', productImg: "./upload/product2.png");
+            component(productName:"Product3", productPrice:'$599', productImg: "./upload/product3.png");
+            component(productName:"Product4", productPrice:'$799', productImg: "./upload/product4.png");
+        ?>
     </div>
 </div>
     
---
+++ php/Database.php
@@ -0,0 +1,71 @@
+<?php
+
+class Database
+{
+    public $serverName;
+    public $userName;
+    public $password;
+    public $dbName;
+    public $tableName;
+    public $con;
+
+
+    // class constructor
+    public function __construct(
+        $dbName = "Newdb",
+        $tableName = "Productdb",
+        $serverName = "localhost",
+        $userName = "root",
+        $password = ""
+    )
+    {
+      $this->dbName = $dbName;
+      $this->tableName = $tableName;
+      $this->serverName = $serverName;
+      $this->userName = $userName;
+      $this->password = $password;
+
+      // create connection
+        $this->con = mysqli_connect($serverName, $userName, $password);
+
+        // Check connection
+        if (!$this->con){
+            die("Connection failed : " . mysqli_connect_error());
+        }
+
+        // query
+        $sql = "CREATE DATABASE IF NOT EXISTS $dbName";
+
+        // execute query
+        if(mysqli_query($this->con, $sql)){
+
+            $this->con = mysqli_connect($serverName, $userName, $password, $dbName);
+
+            // sql to create new table
+            $sql = "CREATE TABLE IF NOT EXISTS $tableName
+                    (id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                    product_name VARCHAR (25) NOT NULL,
+                    product_price FLOAT,
+                    product_image VARCHAR (100)
+                    );";
+
+            if (!mysqli_query($this->con, $sql)){
+                echo "Error creating table : " . mysqli_error($this->con);
+            }
+
+        }else{
+            return false;
+        }
+    }
+
+    // get product from the database
+    public function getData(){
+        $sql = "SELECT * FROM $this->tableName";
+
+        $result = mysqli_query($this->con, $sql);
+
+        if(mysqli_num_rows($result) > 0){
+            return $result;
+        }
+    }
+}
